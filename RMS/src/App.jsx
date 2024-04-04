44889aa3dfa29465245183dd8cdb6f7208d01cfb
Kavinda-B-Medagoda <kavindabmedagoda@gmail.com>
2024-04-04 20:33:49 +0530

    app.jsx changed

--- RMS/src/App.jsx
+++ RMS/src/App.jsx
@@ -4,19 +4,7 @@ import "./index.css";
 import Layout from "./layout/Layout";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/api/login" element={<Login />} />
-        <Route path="/api/register" element={<Register />} />
-        <Route path="/api/admin" element={<Admin />} />
-        <Route path="/api/interviewer" element={<Interviewer />} />
-        <Route path="/api/hr_person" element={<HrPerson />} />
-        <Route path="/api/project_manager" element={<ProjectManager />} />
-      </Routes>
-    </Router>
-  );
+  return <Layout />;
 }
 
 export default App;
