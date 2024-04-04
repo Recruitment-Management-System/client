a8b2f8be4a68de26daccfc53749ea4d6d2da0026
HansakaJS <hansakajayawarna@gmail.com>
2024-04-04 06:19:47 +0530

    vacancy create

--- RMS/src/App.jsx
+++ RMS/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './modules/login';
 import './index.css'
 import Register from './modules/register';
+import CreateVacancy from './modules/addVacancy';
+import UpdateVacancy from './modules/updatevacancy';
+import Project from './modules/projects';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/create-vacancy/:projectID" element={<CreateVacancy />}/>
+        <Route path="/update-vacancy" element={<UpdateVacancy />} />
+        <Route path="/projects" element={<Project />} />
       </Routes>
     </Router>
   );
