8a0aaaed3ab360d6b91606b5dd705c970e4284d8
CharuniPremarathne <kpremarathne996@gmail.com>
2024-04-16 06:25:47 +0530

    navbar rendering

--- RMS/src/App.jsx
+++ RMS/src/App.jsx
@@ -11,88 +11,36 @@ import Interviewer from "./pages/Interviewer";
 import HrPerson from "./pages/HrPerson";
 import ProjectManager from "./pages/ProjectManager";
 
-import CreateVacancy from './components/addVacancy';
-import UpdateVacancy from './components/updatevacancy';
-import Project from './components/projects';
+import CreateVacancy from "./components/addVacancy";
+import UpdateVacancy from "./components/updatevacancy";
+import Project from "./components/projects";
 import VacancyTable from "./components/vacancyProject";
-
-
 
 import AddCandidate from "./components/addCandidate";
 import Vacancy from "./components/vacancy";
-
 
 import CandidateInfo from "./components/candidateInformation";
 import CandidateInfoPage from "./components/candidateInformation";
 import Interview from "./components/interview";
 import InterviewsList from "./components/interviewsList";
 
-
-import AddFeedback from './pages/feedback/AddFeedback';
-import ViewFeedbackInterviewer from './pages/feedback/ViewFeedbackInterviewer';
-import ViewFeedbackHR from "./pages/feedback/ViewFeedbackHR";
-import AddFeedbackHR from "./pages/feedback/AddFeedbackHR";
-import CandidateList from "./components/CandidateList";
-import UpdateFeedbackHR from "./pages/feedback/UpdateFeedbackHR";
-import CandidateInterviewsList from "./components/candidateInterviewList";
-
-
 axios.defaults.baseURL = "http://localhost:8080/api";
 // axios.defaults.withCredentials = true;
-
 
 import Layout from "./layout/Layout";
 
 function App() {
-
-
-
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem("token");
 
-    if(token){
+    if (token) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    }else{
+    } else {
       delete axios.defaults.headers.common["Authorization"];
     }
-  }, [])
+  }, []);
 
-
-  return (
-    <Router>
-      <Routes>
-        <Layout/>
-        <Route path="/" element={<Home />} />
-        <Route path="/api/login" element={<Login />} />
-        <Route path="/api/register" element={<Register />} />
-        <Route path="/api/admin" element={<Admin />} />
-        <Route path="/api/interviewer" element={<Interviewer />} />
-        <Route path="/api/hr_person" element={<HrPerson />} />
-        <Route path="/api/project_manager" element={<ProjectManager />} />
-
-       
-        <Route path="/create-vacancy/:projectID" element={<CreateVacancy />}/>
-        <Route path="/update-vacancy/:vacancyID" element={<UpdateVacancy />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/projects/:projectId" element={<VacancyTable />}/>
-
-        <Route path="/candidate" element={<AddCandidate />} />
-        <Route path="/vacancies" element={<Vacancy />} />
-        <Route path="/candidate-information/:id" element={<CandidateInfoPage />} />
-        <Route path="/interview" element={<Interview />} />
-        <Route path="/interviewlist" element={<InterviewsList />} />
-
-        <Route path = "/feedback/savefeedback/:interviewID" element={<AddFeedback/>}/>
-        <Route path = "/feedback/savefeedbackhr/:interviewID" element={<AddFeedbackHR/>}/>
-        <Route path = "/feedback/viewfeedbackin/:interviewid" element={<ViewFeedbackInterviewer/>}/>
-        <Route path = "/feedback/viewfeedbackhr/:interviewid" element={<ViewFeedbackHR/>}/>
-        <Route path = "/candidatelist/:vacancyid" element={<CandidateList/>}/>
-        <Route path = "/feedback/update/:feedbackhrid" element={<UpdateFeedbackHR/>}/>
-        <Route path = "/feedback/candidates/:candidateID" element={<CandidateInterviewsList/>}/>
-      </Routes>
-    </Router>
-  );
-
+  return <Layout />;
 }
 
 export default App;
--- RMS/src/layout/Layout.jsx
+++ RMS/src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "../components/register";
 import Home from "../pages/Home";
@@ -19,10 +19,21 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Candidates from "../components/Candidates";
 
+import AddFeedback from "../pages/feedback/AddFeedback";
+import ViewFeedbackInterviewer from "../pages/feedback/ViewFeedbackInterviewer";
+import ViewFeedbackHR from "../pages/feedback/ViewFeedbackHR";
+import AddFeedbackHR from "../pages/feedback/AddFeedbackHR";
+import CandidateList from "../components/CandidateList";
+import UpdateFeedbackHR from "../pages/feedback/UpdateFeedbackHR";
+import CandidateInterviewsList from "../components/candidateInterviewList";
+
 axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.withCredentials = true;
 
 const Layout = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
@@ -37,7 +48,7 @@ const Layout = () => {
     <Router>
       <div className="h-screen flex w-full">
         <div className="w-1/6 bg-[#222831] h-full overflow-hidden fixed inset-y-0 left-0">
-          <Navbar />
+          <Navbar isAuthenticated={isAuthenticated} />
         </div>
         <div className="flex-1 flex">
           <div className="w-1/6"></div> {/* Empty space for the side navbar */}
@@ -46,7 +57,7 @@ const Layout = () => {
               <Route path="/" element={<Home />} />
               <Route path="/api/register" element={<Register />} />
               <Route path="/api/login" element={<Login />} />
-              <Route path="/api/logout" element={<Logout />} />
+              <Route path="/api/logout" element={<Logout setIsAuthenticated={setIsAuthenticated}/>} />
               <Route path="/api/admin" element={<Admin />} />
               <Route path="/api/interviewer" element={<Interviewer />} />
               <Route path="/api/hr_person" element={<HrPerson />} />
@@ -66,6 +77,35 @@ const Layout = () => {
               <Route path="/vacancies" element={<Vacancy />} />
               <Route path="/add/:vacancyID" element={<AddCandidate />} />
               <Route path="/abc/:vacancyID" element={<Candidates />} />
+
+              <Route
+                path="/feedback/savefeedback/:interviewID"
+                element={<AddFeedback />}
+              />
+              <Route
+                path="/feedback/savefeedbackhr/:interviewID"
+                element={<AddFeedbackHR />}
+              />
+              <Route
+                path="/feedback/viewfeedbackin/:interviewid"
+                element={<ViewFeedbackInterviewer />}
+              />
+              <Route
+                path="/feedback/viewfeedbackhr/:interviewid"
+                element={<ViewFeedbackHR />}
+              />
+              <Route
+                path="/candidatelist/:vacancyid"
+                element={<CandidateList />}
+              />
+              <Route
+                path="/feedback/update/:feedbackhrid"
+                element={<UpdateFeedbackHR />}
+              />
+              <Route
+                path="/feedback/candidates/:candidateID"
+                element={<CandidateInterviewsList />}
+              />
             </Routes>
           </div>
         </div>
