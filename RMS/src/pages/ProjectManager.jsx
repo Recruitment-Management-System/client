3b7d07521b05058aa3dfdb05ec32072c4d1232c1
Kavinda-B-Medagoda <kavindabmedagoda@gmail.com>
2024-04-16 18:00:06 +0530

    dashboards added

--- RMS/src/pages/ProjectManager.jsx
+++ RMS/src/pages/ProjectManager.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const ProjectManager = () => {
   return (
-    <div>ProjectManager</div>
-  )
-}
+    <div className="h-screen overflow-hidden flex w-full">
+      <div
+        className="w-full"
+        style={{
+          backgroundImage: `url('/src/assets/bg.jpg')`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          // backgroundPosition: "center",
+        }}
+      >
+        <div className="h-full overflow-hidden">
+          <h1 className=" text-5xl font-bold text-center pt-10">
+            PROJECT MANAGER DASHBOARD
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectManager
+export default ProjectManager;
