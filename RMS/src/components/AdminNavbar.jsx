ff4cc67d144f9528e6fd89d72fab35016ce2c675
Kavinda-B-Medagoda <kavindabmedagoda@gmail.com>
2024-04-24 20:49:30 +0530

    navbar changed

--- RMS/src/components/AdminNavbar.jsx
+++ RMS/src/components/AdminNavbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const AdminNavbar = () => {
+  const token = localStorage.getItem("token");
+  const decodeToken = jwtDecode(token);
+  const id = decodeToken.id;
+  const userName = decodeToken.sub;
   return (
     <div>
       <div>
+        <h1 className="text-red-500 text-4xl text-center pt-10">ADMIN</h1>
+        <h2 className="text-red-500 text-md text-center">{userName}</h2>
+
         <ul className="ml-5 pt-16 flex flex-col gap-5">
           <li className="text-xl hover:text-blue-500 duration-300">
             {/* <Link>View Vacancies</Link> */}
--- RMS/src/components/InterviewerNavbar.jsx
+++ RMS/src/components/InterviewerNavbar.jsx
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 const InterviewerNavbar = () => {
-
-const token = localStorage.getItem('token');
-const decodeToken = jwtDecode(token);
-const id = decodeToken.id;
-
+  const token = localStorage.getItem("token");
+  const decodeToken = jwtDecode(token);
+  const id = decodeToken.id;
+  const userName = decodeToken.sub;
   return (
     <div>
       <div>
+        <h1 className="text-red-500 text-4xl text-center pt-10">INTERVIEWER</h1>
+        <h2 className="text-red-500 text-md text-center">{userName}</h2>
+
         <ul className="ml-5 pt-16 flex flex-col gap-5">
           <li className="text-xl hover:text-blue-500 duration-300">
-            <Link to = {`/api/interviewer/interviewlist/${id}`}>My Interviews</Link>
+            <Link to={`/api/interviewer/interviewlist/${id}`}>
+              My Interviews
+            </Link>
           </li>
           {/* <li className="text-xl hover:text-blue-500 duration-300">
             <Link>View Feedbacks</Link>
